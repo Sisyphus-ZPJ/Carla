50804f5f60d743a11157d5288bcc7409a547f466
ENight <2724682324@qq.com>
2023-10-13 20:55:25 +0800

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,401 @@
+import carla
+import random
+import logging
+import numpy as np
+
+from carla import ColorConverter as cc
+
+# ==============================================================================
+# -- CameraManager -------------------------------------------------------------
+# ==============================================================================
+
+
+class CameraManager(object):
+    def __init__(self, parent_actor, hud, gamma_correction):
+        self.sensor = None
+        self.surface = None
+        self._parent = parent_actor
+        self.hud = hud
+        self.recording = False
+        bound_x = 0.5 + self._parent.bounding_box.extent.x
+        bound_y = 0.5 + self._parent.bounding_box.extent.y
+        bound_z = 0.5 + self._parent.bounding_box.extent.z
+        Attachment = carla.AttachmentType
+
+        if not self._parent.type_id.startswith("walker.pedestrian"):
+            self._camera_transforms = [
+                (carla.Transform(carla.Location(x=-2.0*bound_x, y=+0.0*bound_y, z=2.0*bound_z), carla.Rotation(pitch=8.0)), Attachment.SpringArmGhost),
+                (carla.Transform(carla.Location(x=+0.8*bound_x, y=+0.0*bound_y, z=1.3*bound_z)), Attachment.Rigid),
+                (carla.Transform(carla.Location(x=+1.9*bound_x, y=+1.0*bound_y, z=1.2*bound_z)), Attachment.SpringArmGhost),
+                (carla.Transform(carla.Location(x=-2.8*bound_x, y=+0.0*bound_y, z=4.6*bound_z), carla.Rotation(pitch=6.0)), Attachment.SpringArmGhost),
+                (carla.Transform(carla.Location(x=-1.0, y=-1.0*bound_y, z=0.4*bound_z)), Attachment.Rigid)]
+        else:
+            self._camera_transforms = [
+                (carla.Transform(carla.Location(x=-2.5, z=0.0), carla.Rotation(pitch=-8.0)), Attachment.SpringArmGhost),
+                (carla.Transform(carla.Location(x=1.6, z=1.7)), Attachment.Rigid),
+                (carla.Transform(carla.Location(x=2.5, y=0.5, z=0.0), carla.Rotation(pitch=-8.0)), Attachment.SpringArmGhost),
+                (carla.Transform(carla.Location(x=-4.0, z=2.0), carla.Rotation(pitch=6.0)), Attachment.SpringArmGhost),
+                (carla.Transform(carla.Location(x=0, y=-2.5, z=-0.0), carla.Rotation(yaw=90.0)), Attachment.Rigid)]
+
+        self.transform_index = 1
+        self.sensors = [
+            ['sensor.camera.rgb', cc.Raw, 'Camera RGB', {}],
+            ['sensor.camera.depth', cc.Raw, 'Camera Depth (Raw)', {}],
+            ['sensor.camera.depth', cc.Depth, 'Camera Depth (Gray Scale)', {}],
+            ['sensor.camera.depth', cc.LogarithmicDepth, 'Camera Depth (Logarithmic Gray Scale)', {}],
+            ['sensor.camera.semantic_segmentation', cc.Raw, 'Camera Semantic Segmentation (Raw)', {}],
+            ['sensor.camera.semantic_segmentation', cc.CityScapesPalette, 'Camera Semantic Segmentation (CityScapes Palette)', {}],
+            ['sensor.camera.instance_segmentation', cc.CityScapesPalette, 'Camera Instance Segmentation (CityScapes Palette)', {}],
+            ['sensor.camera.instance_segmentation', cc.Raw, 'Camera Instance Segmentation (Raw)', {}],
+            ['sensor.lidar.ray_cast', None, 'Lidar (Ray-Cast)', {'range': '50'}],
+            ['sensor.camera.dvs', cc.Raw, 'Dynamic Vision Sensor', {}],
+            ['sensor.camera.rgb', cc.Raw, 'Camera RGB Distorted',
+                {'lens_circle_multiplier': '3.0',
+                'lens_circle_falloff': '3.0',
+                'chromatic_aberration_intensity': '0.5',
+                'chromatic_aberration_offset': '0'}],
+            ['sensor.camera.optical_flow', cc.Raw, 'Optical Flow', {}],
+            ['sensor.camera.normals', cc.Raw, 'Camera Normals', {}],
+        ]
+        world = self._parent.get_world()
+        bp_library = world.get_blueprint_library()
+        for item in self.sensors:
+            bp = bp_library.find(item[0])
+            if item[0].startswith('sensor.camera'):
+                bp.set_attribute('image_size_x', str(hud.dim[0]))
+                bp.set_attribute('image_size_y', str(hud.dim[1]))
+                if bp.has_attribute('gamma'):
+                    bp.set_attribute('gamma', str(gamma_correction))
+                for attr_name, attr_value in item[3].items():
+                    bp.set_attribute(attr_name, attr_value)
+            elif item[0].startswith('sensor.lidar'):
+                self.lidar_range = 50
+
+                for attr_name, attr_value in item[3].items():
+                    bp.set_attribute(attr_name, attr_value)
+                    if attr_name == 'range':
+                        self.lidar_range = float(attr_value)
+
+            item.append(bp)
+        self.index = None
+
+    def toggle_camera(self):
+        self.transform_index = (self.transform_index + 1) % len(self._camera_transforms)
+        self.set_sensor(self.index, notify=False, force_respawn=True)
+
+    def set_sensor(self, index, notify=True, force_respawn=False):
+        index = index % len(self.sensors)
+        needs_respawn = True if self.index is None else \
+            (force_respawn or (self.sensors[index][2] != self.sensors[self.index][2]))
+        if needs_respawn:
+            if self.sensor is not None:
+                self.sensor.destroy()
+                self.surface = None
+            self.sensor = self._parent.get_world().spawn_actor(
+                self.sensors[index][-1],
+                self._camera_transforms[self.transform_index][0],
+                attach_to=self._parent,
+                attachment_type=self._camera_transforms[self.transform_index][1])
+            # We need to pass the lambda a weak reference to self to avoid
+            # circular reference.
+            weak_self = weakref.ref(self)
+            self.sensor.listen(lambda image: CameraManager._parse_image(weak_self, image))
+        if notify:
+            self.hud.notification(self.sensors[index][2])
+        self.index = index
+
+    def next_sensor(self):
+        self.set_sensor(self.index + 1)
+
+    def toggle_recording(self):
+        self.recording = not self.recording
+        self.hud.notification('Recording %s' % ('On' if self.recording else 'Off'))
+
+    def render(self, display):
+        if self.surface is not None:
+            display.blit(self.surface, (0, 0))
+
+    @staticmethod
+    def _parse_image(weak_self, image):
+        self = weak_self()
+        if not self:
+            return
+        if self.sensors[self.index][0].startswith('sensor.lidar'):
+            points = np.frombuffer(image.raw_data, dtype=np.dtype('f4'))
+            points = np.reshape(points, (int(points.shape[0] / 4), 4))
+            lidar_data = np.array(points[:, :2])
+            lidar_data *= min(self.hud.dim) / (2.0 * self.lidar_range)
+            lidar_data += (0.5 * self.hud.dim[0], 0.5 * self.hud.dim[1])
+            lidar_data = np.fabs(lidar_data)  # pylint: disable=E1111
+            lidar_data = lidar_data.astype(np.int32)
+            lidar_data = np.reshape(lidar_data, (-1, 2))
+            lidar_img_size = (self.hud.dim[0], self.hud.dim[1], 3)
+            lidar_img = np.zeros((lidar_img_size), dtype=np.uint8)
+            lidar_img[tuple(lidar_data.T)] = (255, 255, 255)
+            self.surface = pygame.surfarray.make_surface(lidar_img)
+        elif self.sensors[self.index][0].startswith('sensor.camera.dvs'):
+            # Example of converting the raw_data from a carla.DVSEventArray
+            # sensor into a NumPy array and using it as an image
+            dvs_events = np.frombuffer(image.raw_data, dtype=np.dtype([
+                ('x', np.uint16), ('y', np.uint16), ('t', np.int64), ('pol', np.bool)]))
+            dvs_img = np.zeros((image.height, image.width, 3), dtype=np.uint8)
+            # Blue is positive, red is negative
+            dvs_img[dvs_events[:]['y'], dvs_events[:]['x'], dvs_events[:]['pol'] * 2] = 255
+            self.surface = pygame.surfarray.make_surface(dvs_img.swapaxes(0, 1))
+        elif self.sensors[self.index][0].startswith('sensor.camera.optical_flow'):
+            image = image.get_color_coded_flow()
+            array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+            array = np.reshape(array, (image.height, image.width, 4))
+            array = array[:, :, :3]
+            array = array[:, :, ::-1]
+            self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+        else:
+            image.convert(self.sensors[self.index][1])
+            array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+            array = np.reshape(array, (image.height, image.width, 4))
+            array = array[:, :, :3]
+            array = array[:, :, ::-1]
+            self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+        if self.recording:
+            image.save_to_disk('_out/%08d' % image.frame)
+
+def get_actor_blueprints(world, filter, generation):
+    bps = world.get_blueprint_library().filter(filter)
+
+    if generation.lower() == "all":
+        return bps
+
+    # If the filter returns only one bp, we assume that this one needed
+    # and therefore, we ignore the generation
+    if len(bps) == 1:
+        return bps
+
+    try:
+        int_generation = int(generation)
+        # Check if generation is in available generations
+        if int_generation in [1, 2]:
+            bps = [x for x in bps if int(x.get_attribute('generation')) == int_generation]
+            return bps
+        else:
+            print("   Warning! Actor Generation is not valid. No actor will be spawned.")
+            return []
+    except:
+        print("   Warning! Actor Generation is not valid. No actor will be spawned.")
+        return []
+    
+
+def main():
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    seed = 0
+    seed_walk = 0
+    synchronous = True
+    hero = True
+    number_of_vehicles = 30
+    number_of_walkers = 10
+    car_lights_on = False
+
+    random.seed(seed)
+
+    vehicles_list = []
+    walkers_list = []
+    all_id = []
+
+    # Load Client
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(10.0)
+
+    # Load World
+    world = client.get_world()
+    settings = world.get_settings()
+
+    # Load Traffic Manger
+    traffic_manager = client.get_trafficmanager(8000)
+    traffic_manager.set_global_distance_to_leading_vehicle(2.5)
+    traffic_manager.set_random_device_seed(seed)
+
+    # Synchronous or Asynchronous
+    synchronous_master = synchronous
+    if synchronous:
+        traffic_manager.set_synchronous_mode(True)
+        if not settings.synchronous_mode:
+            synchronous_master = True
+            settings.synchronous_mode = True
+            settings.fixed_delta_seconds = 0.05
+        else:
+            synchronous_master = False
+    world.apply_settings(settings)
+
+    # Load Blueprints
+    blueprintsVehicle = get_actor_blueprints(world, 'vehicle.*', 'All')
+    blueprintsWalkers = get_actor_blueprints(world, 'walker.pedestrian.*', '2')
+    blueprintsVehicle = sorted(blueprintsVehicle, key=lambda bp: bp.id)
+
+    # Load Spawn Points
+    spawn_points = world.get_map().get_spawn_points()
+    number_of_spawn_points = len(spawn_points)
+    if number_of_vehicles < number_of_spawn_points:
+        random.shuffle(spawn_points)
+    elif number_of_vehicles > number_of_spawn_points:
+        msg = 'requested %d vehicles, but could only find %d spawn points'
+        logging.warning(msg, number_of_vehicles, number_of_spawn_points)
+        number_of_vehicles = number_of_spawn_points
+
+    # @todo cannot import these directly.
+    SpawnActor = carla.command.SpawnActor
+    SetAutopilot = carla.command.SetAutopilot
+    FutureActor = carla.command.FutureActor
+
+    # --------------
+    # Spawn vehicles
+    # --------------
+    batch = []
+    for n, transform in enumerate(spawn_points):
+        if n >= number_of_vehicles:
+            break
+        blueprint = random.choice(blueprintsVehicle)
+        if blueprint.has_attribute('color'):
+            color = random.choice(blueprint.get_attribute('color').recommended_values)
+            blueprint.set_attribute('color', color)
+        if blueprint.has_attribute('driver_id'):
+            driver_id = random.choice(blueprint.get_attribute('driver_id').recommended_values)
+            blueprint.set_attribute('driver_id', driver_id)
+        if hero:
+            blueprint.set_attribute('role_name', 'hero')
+            hero = False
+        else:
+            blueprint.set_attribute('role_name', 'autopilot')
+
+        # spawn the cars and set their autopilot and light state all together
+        batch.append(SpawnActor(blueprint, transform)
+            .then(SetAutopilot(FutureActor, True, traffic_manager.get_port())))
+
+    for response in client.apply_batch_sync(batch, synchronous_master):
+        if response.error:
+            logging.error(response.error)
+        else:
+            vehicles_list.append(response.actor_id)
+    
+    # Set automatic vehicle lights update if specified
+    if car_lights_on:
+        all_vehicle_actors = world.get_actors(vehicles_list)
+        for actor in all_vehicle_actors:
+            traffic_manager.update_vehicle_lights(actor, True)
+    
+    # -------------
+    # Spawn Walkers
+    # -------------
+    # some settings
+    percentagePedestriansRunning = 0.0      # how many pedestrians will run
+    percentagePedestriansCrossing = 0.0     # how many pedestrians will walk through the road
+    if seed_walk:
+        world.set_pedestrians_seed(seed_walk)
+        random.seed(seed_walk)
+    # 1. take all the random locations to spawn
+    spawn_points = []
+    for i in range(number_of_walkers):
+        spawn_point = carla.Transform()
+        loc = world.get_random_location_from_navigation()
+        if (loc != None):
+            spawn_point.location = loc
+            spawn_points.append(spawn_point)
+    # 2. we spawn the walker object
+    batch = []
+    walker_speed = []
+    for spawn_point in spawn_points:
+        walker_bp = random.choice(blueprintsWalkers)
+        # set as not invincible
+        if walker_bp.has_attribute('is_invincible'):
+            walker_bp.set_attribute('is_invincible', 'false')
+        # set the max speed
+        if walker_bp.has_attribute('speed'):
+            if (random.random() > percentagePedestriansRunning):
+                # walking
+                walker_speed.append(walker_bp.get_attribute('speed').recommended_values[1])
+            else:
+                # running
+                walker_speed.append(walker_bp.get_attribute('speed').recommended_values[2])
+        else:
+            print("Walker has no speed")
+            walker_speed.append(0.0)
+        batch.append(SpawnActor(walker_bp, spawn_point))
+    results = client.apply_batch_sync(batch, True)
+    walker_speed2 = []
+    for i in range(len(results)):
+        if results[i].error:
+            logging.error(results[i].error)
+        else:
+            walkers_list.append({"id": results[i].actor_id})
+            walker_speed2.append(walker_speed[i])
+    walker_speed = walker_speed2
+    # 3. we spawn the walker controller
+    batch = []
+    walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
+    for i in range(len(walkers_list)):
+        batch.append(SpawnActor(walker_controller_bp, carla.Transform(), walkers_list[i]["id"]))
+    results = client.apply_batch_sync(batch, True)
+    for i in range(len(results)):
+        if results[i].error:
+            logging.error(results[i].error)
+        else:
+            walkers_list[i]["con"] = results[i].actor_id
+    # 4. we put together the walkers and controllers id to get the objects from their id
+    for i in range(len(walkers_list)):
+        all_id.append(walkers_list[i]["con"])
+        all_id.append(walkers_list[i]["id"])
+    all_actors = world.get_actors(all_id)
+
+    # wait for a tick to ensure client receives the last transform of the walkers we have just created
+    if not synchronous or not synchronous_master:
+        world.wait_for_tick()
+    else:
+        world.tick()
+
+    # 5. initialize each controller and set target to walk to (list is [controler, actor, controller, actor ...])
+    # set how many pedestrians can cross the road
+    world.set_pedestrians_cross_factor(percentagePedestriansCrossing)
+    for i in range(0, len(all_id), 2):
+        # start walker
+        all_actors[i].start()
+        # set walk to random point
+        all_actors[i].go_to_location(world.get_random_location_from_navigation())
+        # max speed
+        all_actors[i].set_max_speed(float(walker_speed[int(i/2)]))
+
+    print('spawned %d vehicles and %d walkers, press Ctrl+C to exit.' % (len(vehicles_list), len(walkers_list)))
+
+    # Example of how to use Traffic Manager parameters
+    traffic_manager.global_percentage_speed_difference(30.0)
+    
+    # -------------
+    # Sensors
+    # -------------
+    main_vehicle = world.get_actor(random.choice(vehicles_list))
+    camera_init_trans = carla.Transform(carla.Location(z=1.5))
+
+    camera_rgb = world.get_blueprint_library().find('sensor.camera.rgb')
+    sensor_rgb = world.spawn_actor(camera_rgb, camera_init_trans, main_vehicle)
+    sensor_rgb.listen(lambda image: image.save_to_disk('C:\\Carla\\out\\%06d.png' % image.frame))
+
+    # camera_dvs = world.get_blueprint_library().find('sensor.camera.dvs')
+    # sensor_dvs = world.spawn_actor(camera_dvs, camera_init_trans, main_vehicle)
+    # sensor_dvs.listen(lambda image: image.save_to_disk('C:\\Carla\\out\\%06d.png' % image.frame))
+
+    # camera_optical_flow = world.get_blueprint_library().find('sensor.camera.optical_flow')
+    # sensor_optical_flow = world.spawn_actor(camera_optical_flow, camera_init_trans, main_vehicle)
+    # sensor_optical_flow.listen(lambda image: image.save_to_disk('C:\\Carla\\out\\%06d.png' % image.frame))
+
+    # -------------
+    # Main Loop
+    # -------------
+    while True:
+        if synchronous and synchronous_master:
+            world.tick()
+        else:
+            world.wait_for_tick()
+
+
+    
+
+if __name__ == "__main__":
+    main()
